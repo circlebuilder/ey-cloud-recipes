38b5efd795c0c1295b288bf1335236b0960f4d38
Ryan Fogle <fogle@atomicobject.com>
2010-08-17 14:55:07 -0400

    added nginx recipe

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,18 @@
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+
+  # be sure to replace "app_name" with the name of your application.
+  run_for_app("CircleBuilder2") do |app_name, data|
+    template "/etc/nginx/servers/keep.#{app_name}.conf" do
+      source "nginx.erb"
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      backup false
+      variables({
+        :app_name => app_name,
+        :user => node[:owner_name], 
+        :framework_env => node[:environment][:framework_env]
+      })
+    end
+  end
+end
