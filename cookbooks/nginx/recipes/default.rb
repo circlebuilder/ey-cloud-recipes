87e57bcb094cfa025af270c895db9e1d1203a587
Ryan Fogle <fogle@atomicobject.com>
2010-08-17 14:58:23 -0400

    added Jewishbuilder

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,7 +1,7 @@
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
 
   # be sure to replace "app_name" with the name of your application.
-  run_for_app("CircleBuilder2") do |app_name, data|
+  run_for_app("CircleBuilder2", "JewishBuilder") do |app_name, data|
     template "/etc/nginx/servers/keep.#{app_name}.conf" do
       source "nginx.erb"
       owner node[:owner_name]
