40f18662b92f8eaabfed2912062061d3ee0d31e8
Ryan Fogle <fogle@atomicobject.com>
2010-08-27 14:26:42 -0400

    added custom stylesheet support in https

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,9 +1,22 @@
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
 
   # be sure to replace "app_name" with the name of your application.
-  run_for_app("CircleBuilder2", "JewishBuilder") do |app_name, data|
+  run_for_app("CircleBuilder2", "JewishBuilder", "CatholicBuilder") do |app_name, data|
     template "/etc/nginx/servers/#{app_name}.conf" do
       source "nginx.erb"
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      backup false
+      variables({
+        :app_name => app_name,
+        :user => node[:owner_name], 
+        :framework_env => node[:environment][:framework_env]
+      })
+    end
+    
+    template "/etc/nginx/servers/#{app_name}.ssl.conf" do
+      source "nginx.ssl.erb"
       owner node[:owner_name]
       group node[:owner_name]
       mode 0644
